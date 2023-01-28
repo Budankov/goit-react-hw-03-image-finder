5afcc7f50d0863ffc6149aeec40e2e42ebeb7081
Budankov Kostya <107200706+Budankov@users.noreply.github.com>
2023-01-28 14:58:29 +0200

    Update

---
+++ src/modules/SearchImages/SearchImages.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class SearchImages extends Component {
+  render() {
+    return (
+      <>
+        <input placeholder="Search Images" />
+      </>
+    );
+  }
+}
+
+export default SearchImages;
