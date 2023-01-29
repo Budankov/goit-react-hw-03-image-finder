f15477ad22603c2011cf3bf4a21544b8701288d7
Budankov Kostya <107200706+Budankov@users.noreply.github.com>
2023-01-29 18:35:27 +0200

    Update

--- src/modules/SearchImages/ImageNotFound/ImageNotFound.jsx
+++ src/modules/SearchImages/ImageNotFound/ImageNotFound.jsx
@@ -3,7 +3,10 @@ import styles from './image-not-found.module.scss';
 const ImageNotFound = () => {
   return (
     <div className={styles.ImageNotFound}>
-      <img src="https://fisnikde.com/wp-content/uploads/2019/01/broken-image.png" />
+      <img
+        src="https://fisnikde.com/wp-content/uploads/2019/01/broken-image.png"
+        alt=""
+      />
     </div>
   );
 };
